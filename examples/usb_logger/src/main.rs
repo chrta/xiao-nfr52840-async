6fedac8a521d96cb5cbaf360afe65b652555d18d
Christian Taedcke <hacking@taedcke.com>
2022-11-21 17:33:49 +0100

    usb-logger: Use external xtal and cargo fmt

--- examples/usb_logger/src/main.rs
+++ examples/usb_logger/src/main.rs
@@ -2,16 +2,9 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use xiao_nrf52840_async::*;
-
 use defmt_rtt as _;
 use embassy_executor::Spawner;
-use embassy_nrf::{
-    interrupt,
-    peripherals::USBD,
-    usb,
-    usb::{PowerUsb, UsbSupply},
-};
+use embassy_nrf::{config, interrupt, peripherals::USBD, usb, usb::PowerUsb};
 use embassy_time::{Duration, Timer};
 use panic_probe as _;
 
@@ -22,7 +15,11 @@ async fn logger_task(driver: usb::Driver<'static, USBD, PowerUsb>) {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
+    let mut config: config::Config = Default::default();
+    config.hfclk_source = config::HfclkSource::ExternalXtal;
+
+    let p = embassy_nrf::init(config);
+
     let irq = interrupt::take!(USBD);
     let power_irq = interrupt::take!(POWER_CLOCK);
     let driver = usb::Driver::new(p.USBD, irq, PowerUsb::new(power_irq));
